4f872808dbf512f25981475bd56f6c8bfa0d6742
foysal90 <foysal1416@hotmail.com>
2023-12-05 22:45:53 -0500

    header created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <Header/>
+   
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Header.css'
+import logo from '../../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+           <div className=''>
+           <a href="/shop">shop</a>
+            <a href="/order">order</a>
+            <a href="/inventory">inventory</a>
+            <a href="/login">login</a>
+           </div>
+        </nav>
+    );
+};
+
+export default Header;
