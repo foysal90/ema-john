b70789223c0e24db589687beddecd3415b7a1885
foysal90 <foysal1416@hotmail.com>
2023-12-07 08:55:54 -0500

    icon added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,32 +1,39 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
- 
+  console.log(props);
 
   // eslint-disable-next-line react/prop-types, no-unused-vars
   const { name, category, price, ratings, seller, img, quantity } =
-    // eslint-disable-next-line react/prop-types
     props.product;
+  // eslint-disable-next-line react/prop-types
+  const handleAddToCart = props.handleAddToCart;
   return (
-    
     <div className="product">
       <img src={img} alt="" />
-     <div className="product-info">
-     <h3> {name} </h3>
-      <h4> {category}</h4>
-      <h4>Seller : {seller}</h4>
-      <h4> Rating : {ratings}</h4>
-      <h4> Price : ${price}</h4>
-     </div>
-      <button className="btn-cart">Add To Cart</button>
+      <div className="product-info">
+        <h3> {name} </h3>
+        <h4> {category}</h4>
+        <h4>Seller : {seller}</h4>
+        <h4> Rating : {ratings}</h4>
+        <h4> Price : ${price}</h4>
+      </div>
+
+      <button
+        onClick={() => {
+          handleAddToCart(props.product);
+        }}
+        className="btn-cart"
+      >
+        Add To Cart
+        <FontAwesomeIcon className="icon" icon={faShoppingCart} />
+      </button>
     </div>
-
-    
-    
-
-  
   );
 };
 
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,6 +5,9 @@ import Product from "../Product/Product";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [cart, setCart] = useState([])
+//   cart.push()
 
   useEffect(() => {
     fetch("products.json")
@@ -12,16 +15,25 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  const handleAddToCart = (product) => {
+    const newCart =[...cart,product];
+    setCart(newCart);
+  }
+
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map(product => 
-          <Product product={product} key={product.id} />
+          <Product product={product} key={product.id}
+          handleAddToCart={handleAddToCart}
+          
+          />
         )}
       </div>
 
       <div className="cart-container">
         <h1> Order Summary</h1>
+        <p>Selected Items : {cart.length}</p>
       </div>
     </div>
   );
